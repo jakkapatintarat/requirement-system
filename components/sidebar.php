e240598fa72a0b00007307407b6852a50ac924bf
Don_jakkapat <79179631+jakkapatintarat@users.noreply.github.com>
2024-03-28 21:39:46 +0700

    Update sidebar.php

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,3 +1,9 @@
+<?php
+require_once './classes/system.php';
+
+$system = new System();
+$systems = $system->show();
+?>
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
@@ -30,15 +36,20 @@
                     </svg>
                 </button>
                 <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                    <?php foreach( $systems as $item ) : ?>
                     <li>
-                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Products</a>
+                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                            <?php echo $item['system_name'] ?>
+                        </a>
                     </li>
-                    <li>
+
+                    <?php endforeach; ?>
+                                        <!-- <li>
                         <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Billing</a>
                     </li>
                     <li>
                         <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
-                    </li>
+                    </li> -->
                 </ul>
             </li>
             <li>
