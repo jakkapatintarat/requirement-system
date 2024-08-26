a5082101d3de2aeff758570ffde7d00f81aa2edf
Don_jakkapat <79179631+jakkapatintarat@users.noreply.github.com>
2024-08-26 07:40:42 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,13 @@
 <?php
-$host = 'localhost';
-$db = 'requirement_system';
-$username = 'root';
-$password = '';
+// $host = 'localhost';
+// $db = 'requirement_system';
+// $username = 'root';
+// $password = '';
+
+$host = 'k3xio06abqa902qt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	';
+$db = 'qkohpzzxolcaccqk';
+$username = 'gisbn60i7vltgfan';
+$password = 's4oazrh8ishg1ge9';
 
 try {
     $dsn = "mysql:host=$host;dbname=$db";
